2c6cae12a963eb89b1664a59a1e7b7925c839593
starman <faizan.humayun@simplifyvms.com>
2024-09-21 02:30:24 +0500

    submission

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,6 +67,10 @@ class User extends Authenticatable
         return $this->hasOne(Consultant::class);
     }
 
+    public function getFullNameAttribute()
+    {
+        return trim($this->first_name . ' ' . ($this->middle_name ?? '') . ' ' . $this->last_name);
+    }
 
 
 }
--- routes/vendor.php
+++ routes/vendor.php
@@ -12,7 +12,9 @@ Route::middleware(['user_role:vendor'])->group(function () {
         Route::resource('career-opportunities', \App\Http\Controllers\Vendor\CareerOpportunitiesController::class);
         Route::get('submission/{id}/create', [SubmissionController::class, 'create'])->name('submission.create');
         Route::post('submission/store', [SubmissionController::class, 'store'])->name('submission.store');
-
+        Route::match(['get', 'post'], 'submission/index', [SubmissionController::class, 'index'])->name('submission.index');
+        Route::get('/submission-list', [SubmissionController::class, 'getSubmissions'])->name('submission.list');
+        Route::get('/submission/{id}', [SubmissionController::class, 'show'])->name('submission.show');
     });
 
 });
