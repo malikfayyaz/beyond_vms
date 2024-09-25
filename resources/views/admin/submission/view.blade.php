bc055e8a4b8b123f1c3190a507849083fb7da893
Zoobia Humayun <zoobia@simplifyvms.com>
2024-09-25 21:32:03 +0500

    add changes

--- resources/views/admin/submission/view.blade.php
+++ resources/views/admin/submission/view.blade.php
@@ -16,12 +16,12 @@
               >
                 schedule interview
               </button>
-              <button
-                type="button"
+              
+              <a href="{{ route('admin.offer.create',  ['id' => 1]) }}"
                 class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 capitalize"
               >
                 create offer
-              </button>
+              </a>
               <button
                 type="button"
                 class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 capitalize"
