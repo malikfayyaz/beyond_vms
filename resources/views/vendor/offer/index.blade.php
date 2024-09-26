6f664319ff2b16306a86d63a646a1b30ba69bce4
starman <faizan.humayun@simplifyvms.com>
2024-09-26 05:59:54 +0500

    ven-offr

---
+++ resources/views/vendor/offer/index.blade.php
@@ -0,0 +1,106 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <!-- Sidebar -->
+    @include('admin.layouts.partials.dashboard_side_bar')
+    <div class="ml-16">
+        @include('admin.layouts.partials.header')
+        <div class="bg-white mx-4 my-8 rounded p-8">
+            <div >
+                <div class="flex justify-between items-center mb-6">
+                    <h2 class="text-2xl font-bold">Offers</h2>
+                </div>
+                <table class="min-w-full divide-y divide-gray-200" id="listing">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <!-- Status -->
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Status
+                        </th>
+                        <!-- User -->
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Offer ID
+                        </th>
+                        <!-- job -->
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Contractor Name
+                        </th>
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Job Profile 
+                        </th>
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Hiring Manger
+                        </th>
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Vendor
+                        </th>
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Offer Date
+                        </th>
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Bill Rate
+                        </th>
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Workorder Status
+                        </th>
+                        <th
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Woker Type
+                        </th>
+                        <th style="width: 80px"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Action
+                        </th>
+                        
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+
+
+            </div>
+        </div>
+    </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        if (window.$) {
+            initializeDataTable('#listing', '/vendor/offer/index', [
+                { data: 'status', name: 'status' },
+                { data: 'id', name: 'id' },
+                { data: 'consultant_name', name: 'consultant_name' }, 
+                { data: 'career_opportunity', name: 'career_opportunity' },
+                { data: 'hiring_manger', name: 'hiring_manger' },
+                { data: 'vendor_name', name: 'vendor_name' },
+                { data: 'created_at', name: 'created_at' },
+                { data: 'offer_bill_rate', name: 'offer_bill_rate' },
+                { data: 'wo_status', name: 'wo_status' },
+                { data: 'worker_type', name: 'worker_type' },
+                { data: 'action', name: 'action', orderable: false, searchable: false }
+               
+            ]);
+        }
+    });
+</script>
+@endsection
