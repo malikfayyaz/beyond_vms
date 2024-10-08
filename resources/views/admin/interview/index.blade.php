2fe439c803df052b0d49e773d5f6585939361b44
starman <faizan.humayun@simplifyvms.com>
2024-10-09 02:03:47 +0500

    spell set

--- resources/views/admin/interview/index.blade.php
+++ resources/views/admin/interview/index.blade.php
@@ -69,14 +69,14 @@
                         <th
                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                         >
-                            End Time 
+                            End Time
                         </th>
                         <th
                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                         >
-                            Woker Type
+                            Worker Type
                         </th>
-                        <th 
+                        <th style="width: 80px"
                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                         >
                             Action
