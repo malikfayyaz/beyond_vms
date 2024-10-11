01a0b09f7941bd8362e8e0b10c550c0974fdd788
starman <faizan.humayun@simplifyvms.com>
2024-10-11 22:24:43 +0500

    intrview view

--- app/Models/CareerOpportunitiesInterviewDate.php
+++ app/Models/CareerOpportunitiesInterviewDate.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class CareerOpportunitiesInterviewDate extends Model
 {
@@ -20,5 +21,16 @@ class CareerOpportunitiesInterviewDate extends Model
     public function interview()
     {
         return $this->belongsTo(CareerOpportunitiesInterview::class, 'interview_id', 'id');
+    }
+
+    public function scopePrimaryDate($query)
+    {
+        $primaryDate = $query->where('schedule_date_order', 1)->first();
+        
+        if ($primaryDate) {
+            $primaryDate->schedule_date = Carbon::parse($primaryDate->schedule_date)->format('m/d/Y');
+        }
+
+        return $primaryDate;
     }
 }
