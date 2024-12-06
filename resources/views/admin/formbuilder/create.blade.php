e603bb82341cb9267306f4d2c4c18068239256d9
starman <faizan.humayun@simplifyvms.com>
2024-12-06 21:48:58 +0500

    Cus-fields-fb

--- resources/views/admin/formbuilder/create.blade.php
+++ resources/views/admin/formbuilder/create.blade.php
@@ -4,6 +4,43 @@
     @include('admin.layouts.partials.dashboard_side_bar')
 
     <div class="ml-16">
+        <div class="flex space-x-4 mx-4 my-4">
+            <div class="flex-1">
+                <label for="formType" class="block mb-2"
+                >Form Type:
+                <span class="text-red-500">*</span></label
+                >
+                <select
+                    id="formType"
+                    x-model="formData.formType"
+                    class="w-full px-3 py-2 border rounded-md"
+                    :class="{'border-red-500': errors.formType}"
+                >
+                    <option value="" disabled selected>Select</option>
+                    @foreach (formType() as $key => $value)
+                        <option value="{{ $key }}">{{ $value }}</option>
+                    @endforeach
+                </select>
+                <p
+                class="text-red-500 text-sm mt-1"
+                x-text="errors.formType"
+                ></p>
+            </div>
+            <div class="flex-1">
+                <label for="formStatus" class="block mb-2"
+                >Status:</label
+                >
+                <select id="formStatus" x-model="formData.formStatus" class="w-full p-2 border rounded h-10">
+                    <option value="" disabled selected>Select Status</option>
+                    <option value="active">Active</option>
+                    <option value="inactive">Inactive</option>
+                </select>
+                <p
+                class="text-red-500 text-sm mt-1"
+                x-text="errors.formStatus"
+                ></p>
+            </div>
+        </div>
         <div id="fb-editor" class="mx-4 my-4"></div>
     </div>
 
