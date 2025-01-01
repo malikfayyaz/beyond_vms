705ca6339227f477df734619378000e236733260
starman <faizan.humayun@simplifyvms.com>
2025-01-01 21:45:49 +0500

    sub-chkbox

--- app/Http/Controllers/Vendor/CareerOpportunitiesSubmissionController.php
+++ app/Http/Controllers/Vendor/CareerOpportunitiesSubmissionController.php
@@ -162,7 +162,8 @@ class CareerOpportunitiesSubmissionController extends Controller
             } elseif (preg_match('/^textarea-/', $key)) {
                 $dynamicRules[$key] = 'string'; // Rule for textarea inputs
             } elseif (preg_match('/^checkbox-/', $key)) {
-                $dynamicRules[$key] = 'string'; // Rule for checkboxes
+                $dynamicRules[$key] = 'array'; // Validate as an array
+                $dynamicRules["{$key}.*"] = 'in:true,false'; // Rule for checkboxes
             } elseif (preg_match('/^radio-/', $key)) {
                 $dynamicRules[$key] = 'string'; // Rule for radio buttons
             } elseif (preg_match('/^select-/', $key)) {
