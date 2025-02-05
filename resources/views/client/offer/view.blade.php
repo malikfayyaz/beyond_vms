208776fa715b2d1b44ba21911c11773834cf9419
starman <faizan.humayun@simplifyvms.com>
2025-02-06 01:22:36 +0500

    offer-FB-label

--- resources/views/client/offer/view.blade.php
+++ resources/views/client/offer/view.blade.php
@@ -417,10 +417,21 @@
                         >
                       </h3>
                     </div>
+                    @php
+                        $fieldLabels = [
+                            // 'name-field' => 'New name'
+                            'text-FB' => 'Text Field',
+                            'date-FB' => 'Date Field',
+                            'number-FB' => 'Num Field',
+                            'select-FB' => 'Option Field',
+                            'textarea-FB' => 'Textarea Field',
+                            // Add more mappings as needed
+                        ];
+                    @endphp
                     @foreach ($offerDetails as $key => $value)
                       <div class="flex items-center justify-between py-4 border-t">
                         <div class="w-2/4">
-                          <h4 class="font-medium">{{ $key }}:</h4>
+                          <h4 class="font-medium">{{ $fieldLabels[$key] ?? ucfirst(str_replace('_', ' ', $key)) }}:</h4>
                         </div>
                         <div class="w-2/4">
                           @if(is_array($value))
